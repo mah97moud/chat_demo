e1eab65b654f6f3d6d64986c3918424dcb3d7be3
Mahmoud Atef <mahmoudatif54@yahoo.com>
2021-03-20 09:54:03 +0200

    update profile Information screen.

--- lib/modules/test_screen/cubit/states.dart
+++
@@ -1,9 +0,0 @@
-abstract class TestStates {}
-
-class InitTestSt extends TestStates {}
-
-class SendTestSt extends TestStates {}
-
-class SucTestSt extends TestStates {}
-
-class ErrTestSt extends TestStates {}
